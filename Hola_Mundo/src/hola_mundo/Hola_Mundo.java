6d18e596ccaf0e8498de0d0ac4041593461984dd
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2024-10-11 21:20:06 -0600

    1er Programa Hola Mundo

---
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hola_mundo;
+
+/**
+ *
+ * @author Oscar Andrade
+ */
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Doble Barra Diagonal es para una linea de comentario
+        /*
+            Conjunto de Lineas para hacer comentarios, es usar
+            un * seguido de una barra diagonal y se cierra el bloque
+            de comentario en sentido contrario, es decir barra diagonal 
+            seguido de *.
+        */
+      System.out.println("Hola Mundo");
+      
+    }//Fin Main
+
+}//FIN de Class
