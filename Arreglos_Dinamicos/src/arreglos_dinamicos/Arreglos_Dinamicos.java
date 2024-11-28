b7147e56658fc08ecbcb207447f9052d5c76b9bc
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2024-11-27 18:18:57 -0600

    Arreglos Dinamicos

--- Arreglos_Dinamicos/src/arreglos_dinamicos/Arreglos_Dinamicos.java
+++ Arreglos_Dinamicos/src/arreglos_dinamicos/Arreglos_Dinamicos.java
@@ -12,28 +12,41 @@ import java.util.ArrayList;
  */
 public class Arreglos_Dinamicos {
 
+    static Scanner scan = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         ArrayList<Integer> notas= new ArrayList<>(); //Tipo entero (int)
-        Scanner scan = new Scanner(System.in);
+        
+        notas = CreacionArray();
+        MostrarResultados(notas);
+        
+
+    }//Fin Main
+    
+    public static ArrayList<Integer> CreacionArray(){
+        ArrayList<Integer> notas= new ArrayList<>(); //Tipo entero (int)
         int notaTemporal = 0;
         
         while(notaTemporal>=0){
+            System.out.println("Ingresar un Numero Negativo para Salir");
             System.out.printf("Ingrese Nota: ");
             notaTemporal = scan.nextInt();
             if(notaTemporal >= 0){
                 notas.add(notaTemporal);
             }
         }//Fin While
-        
-        for(int i = 0 ; i<notas.size();i++ ){
-            System.out.printf("Nota %d: %d\n",i+1,notas.get(i));
+
+        return notas;
+    }//Fin Funcion CreacionArray
+    
+    public static void MostrarResultados(ArrayList<Integer> arreglo){
+        for(int i = 0 ; i<arreglo.size();i++ ){
+            System.out.printf("Nota %d: %d\n",i+1,arreglo.get(i));
+            
         }//Fin For
-                
-        
-    }//Fin Main
+    }//Fin Funcion Mostrar Resultados
     
 }//Fin Class
