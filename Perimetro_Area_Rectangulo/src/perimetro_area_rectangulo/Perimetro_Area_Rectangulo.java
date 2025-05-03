8523105882dd9f61f9e66b3fce22f757f385c69f
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2025-05-02 18:39:17 -0600

    Ejercicio Resuelto

--- Perimetro_Area_Rectangulo/src/perimetro_area_rectangulo/Perimetro_Area_Rectangulo.java
+++ Perimetro_Area_Rectangulo/src/perimetro_area_rectangulo/Perimetro_Area_Rectangulo.java
@@ -4,6 +4,8 @@
  */
 package perimetro_area_rectangulo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Oscar Andrade
@@ -26,27 +28,45 @@ public class Perimetro_Area_Rectangulo {
         
             2. Calcular de un Rectangulo
             2.a. Area.   -> Area = base x altura (b * h)
-            2.B  Periemtro -> Perimetro = lado1 + lado2 + lado3 + lado4
+            2.B  Perimetro -> Perimetro = lado1 + lado2 + lado3 + lado4
                             -> Perimetro = base + altura + base + altura
                             -> Perimetro = 2*base + 2*altura
         
             3. Ingresar Datos por parte del usuario
             3.a Ingresar los lados del rectangulo -> Ingresar la base y la altura
         
-        
             4. Mostrar Resultados
             4.a Mostrar el area del rectangulo
             4.b Mostrar el perimetro del rectangulo
-        
-        
-        
-        
-                 
-        
-                        
-        
-            
         */
+        
+        Scanner scan = new Scanner(System.in);
+        int base = 0;
+        int altura = 0;
+        int perimetro = 0;
+        int area = 0;
+
+        //Ingreso de Datos
+        System.out.println("Ingrese el valor de la altura: ");
+        altura = scan.nextInt();
+        
+        System.out.println("Ingrese el valor de la base: ");
+        base = scan.nextInt();
+        
+        //Calculo de Area
+        area = base * altura;
+        
+        //Calculo de Perimetro
+        perimetro = (2*base) + (2*altura);
+        
+        //Mostrar Resultados
+        System.out.printf("El area del rectangulo es: %d",area);
+        System.out.printf("El perimetro del rectangulo es: %d",perimetro);
+        
+        
+        
+        
+        
     }//Fin Main
     
 }//Fin Class
