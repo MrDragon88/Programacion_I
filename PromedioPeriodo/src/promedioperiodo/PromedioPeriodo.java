174c2528437fac3c4701b3685c1b3cdb9109e8eb
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2025-05-05 18:27:40 -0600

    Estructurad de Seleccion Simple/Doble

---
+++ PromedioPeriodo/src/promedioperiodo/PromedioPeriodo.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package promedioperiodo;
+
+/**
+ *
+ * @author Oscar Andrade
+ */
+public class PromedioPeriodo {
+
+    /**
+     * Programacion Estructurada
+     * 1. Estructura Secuencial -> Despertar, Comer, Aseo Personal, Arreglarse,  y Salir
+     * 2. Estructura Seleccion -> DiaSemana: Vestimenta Formal, sino FindeSemana: VestimentaInformal
+     *      a. Simple
+     *      b. Doble
+     *      c. Anidado
+     *      d. Multiple
+     * 3. Estructura Repeticion ->  Ciclos de Trabajo
+     * 
+     * Programcion Orientada Objetos
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+            < menor
+            > mayor
+            <= menor o igual
+            >= mayor o igual
+            == igual
+            != distinto
+        */
+        
+        if(genero == "Femenino"){
+            //Segmento Verdadero
+            //Ejecuta todo este bloque
+        }
+        else{
+            //Segmento Falso
+            //Ejecuta este bloque
+        }
+        
+        
+        
+        
+    }//Fin Main
+    
+}//Fin Class
