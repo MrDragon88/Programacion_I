e96fffbef4a553cc932ffabba9dd89c4a650c9c9
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2025-05-05 18:53:56 -0600

    Estructura Simple y Doble explicado

--- PromedioPeriodo/src/promedioperiodo/PromedioPeriodo.java
+++ PromedioPeriodo/src/promedioperiodo/PromedioPeriodo.java
@@ -4,6 +4,8 @@
  */
 package promedioperiodo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Oscar Andrade
@@ -32,7 +34,7 @@ public class PromedioPeriodo {
             >= mayor o igual
             == igual
             != distinto
-        */
+        
         
         if(genero == "Femenino"){
             //Segmento Verdadero
@@ -42,9 +44,57 @@ public class PromedioPeriodo {
             //Segmento Falso
             //Ejecuta este bloque
         }
+        */
         
+        /*
+        Elaborar un programa que le permita ingresar el acumulativo 
+        (no más de 50 puntos) y la nota de examen (no más de 50 puntos) 
+        de un estudiante de las tres clases que está llevando y que con 
+        ello pueda calcular el promedio del período. Se muestra en pantalla 
+        el acumulativo y nota de examen de cada una de las clases y el 
+        promedio final.
+        */
+        Scanner scan = new Scanner(System.in);
+        int acumI = 0;
+        int examI = 0;
+        int notaFinalI = 0;
         
+        System.out.print("Clase 1 ->Acumulativo: ");
+        acumI = scan.nextInt();
         
+        //Estructura de Seleccion Simple        
+        if(acumI >50){
+            System.out.println("Acumulativo no puede ser mas de 50");
+            acumI = 50;
+        }
+        
+        System.out.print("Clase 1 ->Examen: ");
+        examI = scan.nextInt();
+        //Estructura de Seleccion Simple
+        if(examI >50){
+            System.out.println("Examen no puede ser mas de 50");
+            examI = 0;
+        }
+        notaFinalI = acumI + examI;
+        
+        System.out.printf("Clase 1 ->Nota Final: %d",notaFinalI);
+        
+           if(notaFinalI >= 70){
+            System.out.println("Aprobado!!!!");
+        }else
+        {
+            System.out.println("Reprobado!!!!!");
+        } //Estructura de Seleccion DOBLE
+    
+        /*
+        OPCION 2
+        if(notaFinalI < 70){
+            System.out.println("Reprobado!!!!");
+        }else
+        {
+            System.out.println("Aprobado!!!!!");
+        }
+        */
         
     }//Fin Main
     
