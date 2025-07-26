ce6d8300bb450f824e379e31cdf7957a37d06b78
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2025-07-25 19:39:50 -0600

    Hola Mundo

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author Oscar Andrade
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        // =>Comentarios en una sola linea - linea individual
+        /*
+            Barra Asterisco - Inicio del Bloque Comentario
+            Asterisco Barra - Cierra el Bloque de Comentario
+        */
+        
+        //Mostrar en Pantalla
+        //Java es sensible a mayusculas y minusculas
+        //Toda sentencia tiene que terminar con punto y coma
+        System.out.println("Hola Mundo");
+        System.out.println("Mi nombre es Oscar!!!");
+        System.out.print("Tengo 99 anios");
+        System.out.print("Mi apellido es: Andrade");
+        /*
+            Caracteres de Escape
+            \n = Nueva Linea
+            \t = Tabulador
+            \r = Retorno de Carro
+            \\ = Diagonal
+            \" = Comilla Doble
+        */
+        System.out.print("\nEstado\n\nCivil:\"Casado\"\tSexo:Masculino");
+      
+        //Hardcoding ->Grabado en el programa
+        
+        //Variables
+        //Java es un lenguaje fuertemente tipado
+        /*
+            Buenas de Programacion
+            a. El nombre de la variable tiene que ser significativo
+            b. Los nombres de las variables tiene una nomenclatura
+                de escritura conocida como camelCase.
+            c. Todas las variables deben de inicializarse
+        */
+        
+        int numeroEntero =100;//Guardar numeros del tipo entero
+        double numeroDecimales=0.0; //Guardar numeros del tipo decimal
+        char caracteres='@'; //letras, digitos, simbolos especiales
+        String palabras="John Doe"; //cadena de caracteres
+        
+    }//Fin de Main
+    
+}//Fin Class
