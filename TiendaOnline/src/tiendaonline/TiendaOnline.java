cbbc3224ace1505a39f6264b285f86dba1c387e3
Oscar Andrade <72425361+MrDragon88@users.noreply.github.com>
2025-08-01 19:40:14 -0600

    Toma de Decision IF/ELSE Operaciones Booleanas AND OR NOT

---
+++ TiendaOnline/src/tiendaonline/TiendaOnline.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiendaonline;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Oscar Andrade
+ */
+public class TiendaOnline {
+
+    /*
+        Proceso de Compra en una Tienda Online
+        Problema: En el proceso de checkout de una tienda online, 
+        se aplican dos reglas de forma separada:
+
+        1. Si el cliente tiene un cupón de descuento, se aplica.
+        2. Si el total de la compra supera los $100, se le notifica 
+           que ha calificado para envío gratis.
+
+        Crear un algoritmo que reciba el total de la compra
+        y si el cliente tiene un cupón. El programa debe aplicar 
+        el descuento si corresponde y luego, en una sección diferente 
+        del código, verificar y notificar sobre el envío gratuito.
+        
+        */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
