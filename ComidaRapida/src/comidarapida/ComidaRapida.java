75cb19de0140fb7e58a2a9fb96498c9a644d5a92
oscar <oscar@OscarAndrade>
2026-01-30 19:31:06 -0600

    Estructura de Selccion tipo multiple Completado el ejercicio

--- ComidaRapida/src/comidarapida/ComidaRapida.java
+++ ComidaRapida/src/comidarapida/ComidaRapida.java
@@ -30,15 +30,17 @@ public class ComidaRapida {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        char seleccionCHAR = '@';
+        char seleccionCHAR = '0';
         int seleccionINT = 0;
         
+        /*
         System.out.println("Comidas Rapidas");
         System.out.println("1. Wendys");
         System.out.println("2. Popeyes");
         System.out.println("3. Dominos");
         System.out.println("4. Trios");
         System.out.println("5. After en el Parque");
+        
         seleccionINT = sc.nextInt();
         
         //Enteros, caracteres, cadenas
@@ -47,21 +49,61 @@ public class ComidaRapida {
                 //Instrucciones de cada Caso / Escenario
                 System.out.println("Hamburguesa para comer....");
                 break;
-            
+
             case 2:
                 System.out.println("Pollo, pollo y mas pollo");
                 break;
-                
+
             case 3:
                 System.out.println("Pizza con todo los ingredientes");
                 System.out.println("sobre todo con piña");
                 break;
-                
+
             case 4:
                 System.out.println("Baleadas con todo!!");
                 break;
-                
+              
             case 5:
+                System.out.println("La fiesta no acaba aun es");
+                System.out.println("es hasta que ......");
+                break;
+                
+            default:
+                System.out.println("Opcion no existe");
+                break;
+        }//Fin de Switch
+   */     
+        //Opciones con CHAR
+        System.out.println("Comidas Rapidas");
+        System.out.println("a. Wendys");
+        System.out.println("b. Popeyes");
+        System.out.println("c. Dominos");
+        System.out.println("d. Trios");
+        System.out.println("e. After en el Parque");
+        //seleccionCHAR = sc.next().charAt(0);
+        seleccionCHAR = sc.next().toLowerCase().charAt(0);//Minusculas
+        //seleccionCHAR = sc.next().toUpperCase().charAt(0);//Mayuscula
+        
+        switch(seleccionCHAR){
+            case 'a':
+                //Instrucciones de cada Caso / Escenario
+                System.out.println("Hamburguesa para comer....");
+                break;
+            
+            case 'b':
+                System.out.println("Pollo, pollo y mas pollo");
+                break;
+             
+            case 'c':
+                System.out.println("Pizza con todo los ingredientes");
+                System.out.println("sobre todo con piña");
+                break;
+             
+            case 'd':
+                System.out.println("Baleadas con todo!!");
+                break;
+              
+            case 'e':
                 System.out.println("La fiesta no acaba aun es");
                 System.out.println("es hasta que ......");
                 break;
